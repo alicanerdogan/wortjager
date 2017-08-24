790f0bcee6289dc8d9f6937c71ebc748afdecbd9
alicanerdogan <aerdogan07@gmail.com>
2017-08-25 01:05:03 +0200

    Fix update script

--- bin/update.sh
+++ bin/update.sh
@@ -4,7 +4,7 @@ source $BASEDIR/env.sh
 git reset --hard origin/master
 git pull origin master
 
-docker build bin/$APPDIR -t $BUILDER_IMAGE_TAG && \
+docker build bin/ -t $BUILDER_IMAGE_TAG && \
 docker run -v $APPDIR:/root/apps $BUILDER_IMAGE_TAG bash -c "cd /root/apps && yarn && yarn build"
 
 cp $APPDIR/build/* /var/www/html/public/wortjager
